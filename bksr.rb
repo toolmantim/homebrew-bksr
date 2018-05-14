3ab2de2466519bd34eed07258d2cefbdfa7371b8
Tim Lucas <t@toolmantim.com>
2018-05-14 23:20:11 +1000

    Add a bksr formula

---
+++ bksr.rb
@@ -0,0 +1,16 @@
+class Bksr < Formula
+  homepage "https://github.com/toolmantim/bksr"
+  url "https://github.com/toolmantim/bksr/releases/download/v2.2.0/bksr-macos.zip"
+  version "2.2.0"
+  sha256 "939b478daa2f690819ccb6174bd5671df648107e96b0d3ed8db0811aeb23f593"
+
+  def install
+    mv "bksr-macos", "bksr"
+    libexec.install Dir['*']
+    bin.install_symlink libexec/"bksr"
+  end
+
+  test do
+    assert_match "bksr #{version}", shell_output("#{bin}/bksr --version 2>&1")
+  end
+end
